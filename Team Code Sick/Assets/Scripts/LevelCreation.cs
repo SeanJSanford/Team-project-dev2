dec2b915af3e762ac1e61dcfbbdf185e66b10bbe
Nilitox-07 <NDGarciaRaudales@student.fullsail.edu>
2026-05-06 21:40:24 -0400

    Level Creation
    Still creating the algorithm, some errors on overlapping rooms, trying to fix it at the moment. Still missing the algorithm to connect the rooms.

--- Team Code Sick/Assets/Scripts/LevelCreation.cs
+++ Team Code Sick/Assets/Scripts/LevelCreation.cs
@@ -23,9 +23,9 @@ public class LevelCreation : MonoBehaviour
     enum Values
     {
         EMPTY,
-        REST,
         TUNNEL,
         WALL,
+        REST,
         FIGHT,
         CHEST,
         STORE,
@@ -35,7 +35,7 @@ public class LevelCreation : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        allPrefabs = new List<GameObject> { restRoomFloor, tunnelFloor, wall, FightRoomFloor, ChestRoomFloor, StoreRoomFloor };
+        allPrefabs = new List<GameObject> { tunnelFloor, wall, restRoomFloor, FightRoomFloor, ChestRoomFloor, StoreRoomFloor };
 
         CreateLevel();
     }
@@ -53,10 +53,12 @@ public class LevelCreation : MonoBehaviour
             List<int> row = new List<int>();
             for (int x = 0; x < size; x++)
             {
-                row.Add(0);
+                row.Add(1);
             }
             grid.Add(row);
         }
+
+        UnityEngine.Random.InitState(0);
 
         StartGrid();
 
@@ -97,7 +99,9 @@ public class LevelCreation : MonoBehaviour
         (int x, int y) SafeAreaSize = (3, 5);
         (int x, int y) FightRoomSize = (5, 5);
         (int x, int y) ChestRoomSize = (3, 3);
-        // (int x, int y) StoreSize = (5, 3);
+        (int x, int y) StoreSize = (5, 3);
+
+        List<(int x, int y)> allSizes = new List<(int x, int y)> { SafeAreaSize, FightRoomSize, ChestRoomSize, StoreSize };
 
         int wallThickness = 1; // This is the walls that go around the room
         int roomSeparation = 1; // This is so the walls are not right next to each other
@@ -115,6 +119,51 @@ public class LevelCreation : MonoBehaviour
 
         bool possibleCenter; // For testing if the center is possible with no overlap
 
+        (int x, int y) roomSize;
+
+        for (int sizeIndex = 0; sizeIndex < allSizes.Count; sizeIndex++)
+        {
+            while (true)
+            {
+                possibleCenter = true;
+
+                roomSize = allSizes[sizeIndex];
+
+                startX = wallThickness + (int)(roomSize.x / 2);
+                endX = size - (int)(roomSize.x / 2) - wallThickness - 1;
+                startY = wallThickness + (int)(roomSize.y / 2);
+                endY = size - (int)(roomSize.y / 2) - wallThickness - 1;
+
+                xPos = UnityEngine.Random.Range(startX, endX); // The 2 is to get the half and getting the floor. The minus 1 in the second param is the index out of bound
+                yPos = UnityEngine.Random.Range(startY, endY);
+
+                (int x, int y) currentCenter = (xPos, yPos);
+                for (int centerToCompare = 0; centerToCompare < allCenters.Count; centerToCompare++)
+                {
+                    (xDiff, yDiff) = ManhattanDistance(allSizes[centerToCompare], currentCenter);
+                    possibleCenter = possibleCenter && (xDiff > (int)(allSizes[centerToCompare].x / 2) + (wallThickness * 2) + roomSeparation + (int)(roomSize.x / 2) || yDiff > (int)(allSizes[centerToCompare].y / 2) + (wallThickness * 2) + roomSeparation + (int)(roomSize.y / 2));
+                }
+                if (possibleCenter)
+                {
+                    for (int y = yPos - startY; y <= yPos + startY; y++)
+                    {
+                        for (int x = xPos - startX; x <= xPos + startX; x++)
+                        {
+                            if (y == yPos - startY || y == yPos + startY || x == xPos - startX || x == xPos + startX)
+                                grid[y][x] = (int)Values.WALL;
+                            else
+                                grid[y][x] = (int)Values.REST + sizeIndex; // sizeIndex will be the current room we are in, it be offset by REST
+                        }
+                    }
+                    allCenters.Add(currentCenter);
+                    List<(int x, int y)> exits = new List<(int x, int y)> { (currentCenter.x - startX, currentCenter.y), (currentCenter.x, currentCenter.y + startX), (currentCenter.x + startX, currentCenter.y), (currentCenter.x, currentCenter.y - startX) };
+                    allExits.Add(exits);
+                    break;
+                }
+            }
+        }
+
+        /*
         startX = wallThickness + (int)(SafeAreaSize.x / 2);
         endX = size - (int)(SafeAreaSize.x / 2) - wallThickness - 1;
         startY = wallThickness + (int)(SafeAreaSize.y / 2);
@@ -222,7 +271,7 @@ public class LevelCreation : MonoBehaviour
         }
 
 
-
+        */
     }
 
     (int x, int y) ManhattanDistance((int x, int y) pos1, (int x, int y) pos2)
