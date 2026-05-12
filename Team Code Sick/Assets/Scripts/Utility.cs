3d9d73fb2995c7b1a00f7a3338b58000b10c6ce5
Nilitox-07 <NDGarciaRaudales@student.fullsail.edu>
2026-05-11 23:06:51 -0400

    Tunnel Algorithm should be fully done. It was tested and it works. Will move things around when we have the player in the scene.

--- Team Code Sick/Assets/Scripts/Utility.cs
+++ Team Code Sick/Assets/Scripts/Utility.cs
@@ -20,18 +20,74 @@ public class Utility : MonoBehaviour
         return (x, y);
     }
 
+    public List<(int x, int y)> ReconstructPath((int x, int y) current, (int x, int y) start, Dictionary<(int x, int y), (int x, int y)> tunnel)
+    {
+        List<(int x, int y)> path = new List<(int x, int y)>();
+
+        while (tunnel.ContainsKey(current))
+        {
+            path.Add(current);
+            current = tunnel[current];
+        }
+        path.Add(start);
+        path.Reverse();
+
+        return path;
+    }
+
     public int AddManhattanDistance((int x, int y) pos1, (int x, int y) pos2)
     {
         (int x, int y) = ManhattanDistance(pos1, pos2);
         return x + y;
     }
 
-    public List<(int x, int y)> AStarAlgorithm((int x, int y) start, (int x, int y) end, int size, List<List<int>> grid, List<int> obstacles)
+    public List<(int x, int y)> AStarAlgorithm((int x, int y) start, (int x, int y) end, int size, List<List<int>> grid, List<int> obstacles, List<(int x, int y)> directions)
     {
-        List<(int cost, (int x, int y))> openSet = new List<(int cost, (int x, int y))>();
+        SimplePriorityQueue<(int x, int y)> openSet = new SimplePriorityQueue<(int x, int y)>();
+
+        openSet.Enqueue(start, 0);
 
         Dictionary<(int x, int y), (int x, int y)> tunnel = new Dictionary<(int x, int y), (int x, int y)>();
 
-        return new List<(int x, int y)>();
+        Dictionary<(int x, int y), int> gScore = new Dictionary<(int x, int y), int>();
+
+        gScore[start] = 0;
+
+        (int x, int y) current;
+        (int x, int y) neighbor;
+
+        int tentativeGScore;
+        int fScore;
+
+        while (openSet.Count > 0)
+        {
+
+            current = openSet.Dequeue();
+
+            if (current == end)
+                return ReconstructPath(current, start, tunnel);
+
+            foreach ((int dx, int dy) in directions)
+            {
+                neighbor = (current.x + dx, current.y + dy);
+
+                if (neighbor.x < 0 || neighbor.x >= size || neighbor.y < 0 || neighbor.y >= size) continue; // Boundary Check
+
+                if (obstacles.Contains(grid[neighbor.y][neighbor.x])) continue; // Check if we should ignore that value
+
+                tentativeGScore = gScore[current] + 1;
+
+                if (!gScore.ContainsKey(neighbor) || tentativeGScore < gScore[neighbor])
+                {
+                    tunnel[neighbor] = current;
+                    gScore[neighbor] = tentativeGScore;
+                    fScore = tentativeGScore + AddManhattanDistance(neighbor, end);
+                    openSet.Enqueue(neighbor, fScore);
+                }
+            }
+
+        }
+
+        return new List<(int x, int y)>(); // No path found
     }
 }
