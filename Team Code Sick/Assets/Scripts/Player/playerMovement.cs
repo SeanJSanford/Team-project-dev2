e3200f3f7565267fe61540bf43088e61270a978f
Dai Nguyen <daiprohp01@gmail.com>
2026-05-25 21:29:13 -0400

    change dashing, add ghost effect

--- Team Code Sick/Assets/Scripts/Player/playerMovement.cs
+++ Team Code Sick/Assets/Scripts/Player/playerMovement.cs
@@ -16,8 +16,11 @@ public class playerMovement : MonoBehaviour, Idamage
     public float sprintMod;
 
     [SerializeField] float dashDist;
-    
     [SerializeField] float dashCooldown;
+    [SerializeField] float dashDuration = 0.15f;
+
+    [SerializeField] GameObject dashGhost;
+    [SerializeField] float ghostSpawnRate = 0.03f;
 
     [SerializeField] Transform gunPivot;
     [SerializeField] Transform shootPos;
@@ -31,6 +34,9 @@ public class playerMovement : MonoBehaviour, Idamage
 
 
     float dashCooldownTimer;
+    bool isDashing;
+
+
     float shootTimer;
     float currentSpeed;
 
@@ -44,6 +50,8 @@ public class playerMovement : MonoBehaviour, Idamage
 
     Vector3 moveDir;
     Vector3 playerVel;
+    Vector3 lastMoveDir;
+
 
     void Start()
     {
@@ -57,14 +65,17 @@ public class playerMovement : MonoBehaviour, Idamage
     // Update is called once per frame
     void Update()
     {
-        
+
         if (!gamemanager.instance.isPaused)
         {
             AimGunAtMouse();
-            Movement();
+            if (!isDashing)
+            {
+                Movement();
+            }
+
+            Dash();
         }
-        // Sprint();
-        Dash();
     }
 
     void Movement()
@@ -78,6 +89,11 @@ public class playerMovement : MonoBehaviour, Idamage
         float z = Input.GetAxisRaw("Vertical");
 
         moveDir = new Vector3(x, 0f, z);
+
+        if (moveDir.sqrMagnitude > 0.01f)
+        {
+            lastMoveDir = moveDir.normalized;
+        }
 
         currentSpeed = speed;
 
@@ -110,33 +126,70 @@ public class playerMovement : MonoBehaviour, Idamage
             Debug.DrawLine(gunPivot.position, mouseWorldPos, Color.green);
         }
     }
-    //void Sprint() //Sprinting with left shift key, increases speed by sprintMod, and returns to normal speed when released
-    //{
-    //    if (Input.GetButtonDown("Sprint"))
-    //    {
-    //        speed *= sprintMod;
-    //    }
-    //    else if (Input.GetButtonUp("Sprint"))
-    //    {
-    //        speed /= sprintMod;
-    //    }
-    //}
+
     void Dash()
     {
         if (dashCooldownTimer > 0)
             dashCooldownTimer -= Time.deltaTime;
 
-        if (Input.GetButtonDown("Dash") && dashCooldownTimer <= 0)
+        if (Input.GetButtonDown("Dash") && dashCooldownTimer <= 0 && !isDashing)
         {
-            Vector3 dashDir = moveDir.normalized;
+            Vector3 dashDir = lastMoveDir;
 
             if (dashDir == Vector3.zero)
             {
-                dashDir = transform.forward;
+                dashDir = gunPivot.forward;
+                dashDir.y = 0f;
+                dashDir.Normalize();
             }
-            controller.Move(dashDir * dashDist);
+            StartCoroutine(DashRoutine(dashDir));
             dashCooldownTimer = dashCooldown;
         }
+    }
+    void SpawnDashGhost()
+    {
+        if (dashGhost == null)
+            return;
+
+        Instantiate(dashGhost, rend.transform.position, rend.transform.rotation);
+    }
+
+
+    IEnumerator DashRoutine(Vector3 dashDir)
+    {
+        isDashing = true;
+
+        float elapsedTime = 0f;
+        float ghostTimer = 0f;
+        float dashSpeed = dashDist / dashDuration;
+
+        while (elapsedTime < dashDuration)
+        {
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+
+            Vector3 inputDir = new Vector3(x, 0f, z);
+
+            if (inputDir.sqrMagnitude > 0.01f)
+            {
+                dashDir = inputDir.normalized;
+            }
+
+            ghostTimer -= Time.deltaTime;
+
+            if (ghostTimer <= 0f)
+            {
+                SpawnDashGhost();
+                ghostTimer = ghostSpawnRate;
+            }
+
+            controller.Move(dashDir * dashSpeed * Time.deltaTime);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        isDashing = false;
     }
 
     void Shoot()
@@ -149,7 +202,7 @@ public class playerMovement : MonoBehaviour, Idamage
 
         Vector3 spawnPos = shootPos.position + shootDir * 0.75f;
 
-        GameObject newProjectile = Instantiate( projectile, spawnPos, Quaternion.LookRotation(shootDir));
+        GameObject newProjectile = Instantiate(projectile, spawnPos, Quaternion.LookRotation(shootDir));
 
         Rigidbody rb = newProjectile.GetComponent<Rigidbody>();
 
