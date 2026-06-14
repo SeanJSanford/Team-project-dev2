d1df3c1399f551c9393a8924b84396e4c05f40fe
Dai Nguyen <daiprohp01@gmail.com>
2026-06-13 22:19:01 -0400

    fix animation

--- Team Code Sick/Assets/Scripts/Player/playerMovement.cs
+++ Team Code Sick/Assets/Scripts/Player/playerMovement.cs
@@ -6,11 +6,15 @@ using System.Collections;
 /// </summary>
 public class playerMovement : MonoBehaviour, Idamage
 {
+
+
     [Header("Sources")]
     [SerializeField] Renderer rend;
     [SerializeField] CharacterController controller;
     [SerializeField] LayerMask ignoreLayer;
-   
+    [SerializeField] Transform visualHolder;
+    [SerializeField] float visualTurnSpeed = 15f;
+    [SerializeField] float visualYawOffset = 0f;
 
     [Header("Audio")]
 
@@ -167,7 +171,22 @@ public class playerMovement : MonoBehaviour, Idamage
 
             if (lookDir.sqrMagnitude > 0.01f)
             {
-                gunPivot.rotation = Quaternion.LookRotation(lookDir);
+                Quaternion aimRotation = Quaternion.LookRotation(lookDir);
+
+              
+                gunPivot.rotation = aimRotation;
+
+              
+                if (visualHolder != null)
+                {
+                    Quaternion visualRotation = aimRotation * Quaternion.Euler(0f, visualYawOffset, 0f);
+
+                    visualHolder.rotation = Quaternion.Slerp(
+                        visualHolder.rotation,
+                        visualRotation,
+                        visualTurnSpeed * Time.deltaTime
+                    );
+                }
             }
 
             Debug.DrawLine(gunPivot.position, mouseWorldPos, Color.green);
@@ -245,6 +264,8 @@ public class playerMovement : MonoBehaviour, Idamage
     void Shoot()
     {
         shootTimer = 0;
+
+
         audPlayer.PlayOneShot(audShoot, audShootVol);
 
         Vector3 shootDir = gunPivot.forward;
