e6a5a9f1e8d9155b766a83400a5e06780ca70bcd
Nilitox-07 <NDGarciaRaudales@student.fullsail.edu>
2026-06-17 14:30:46 -0400

    Started Changing the ICharacter

--- Team Code Sick/Assets/Scripts/Enemy/EnemyLaser.cs
+++ Team Code Sick/Assets/Scripts/Enemy/EnemyLaser.cs
@@ -33,6 +33,7 @@ public class EnemyLaser : MonoBehaviour, Idamage
     public float Resistance { get; set; }
     public bool timerLock { get; set; }
     public float shootRate { get; set; }
+    public ElementType elementType { get; set; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -43,6 +44,7 @@ public class EnemyLaser : MonoBehaviour, Idamage
         Damage = _Damage;
         Resistance = _Resistance;
         shootRate = _shootRate;
+        elementType = LevelCreation.instance.roomElements[gamemanager.instance.currentRoom];
     }
 
     // Update is called once per frame
--- Team Code Sick/Assets/Scripts/Enemy/EnemyRanged.cs
+++ Team Code Sick/Assets/Scripts/Enemy/EnemyRanged.cs
@@ -41,6 +41,7 @@ public class EnemyRanged : MonoBehaviour, Idamage
     public float Resistance { get; set; }
     public bool timerLock { get; set; }
     public float shootRate { get; set; }
+    public ElementType elementType { get ; set; }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -51,6 +52,7 @@ public class EnemyRanged : MonoBehaviour, Idamage
         Damage = _Damage;
         Resistance = _Resistance;
         shootRate = _shootRate;
+        elementType = LevelCreation.instance.roomElements[gamemanager.instance.currentRoom];
     }
 
     // Update is called once per frame
