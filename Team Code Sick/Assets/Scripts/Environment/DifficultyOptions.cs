c66a271a75820b1554f532ad0423f13d53cd95de
Nilitox-07 <NDGarciaRaudales@student.fullsail.edu>
2026-06-21 02:49:12 -0400

    Added the Difficulty Options on the Character Selection Screen. Created another Character Select Screen just in case my push messed up my original Characrter select.

---
+++ Team Code Sick/Assets/Scripts/Environment/DifficultyOptions.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyOptions : MonoBehaviour
+{
+    public static DifficultyOptions instance;
+
+    enum SliderOptions
+    {
+        StartDifficulty,
+        DifficultyRampUp,
+        AmountOfRooms,
+        BossFrequency,
+        ExtractionAmount,
+        Luck,
+        MAX
+    }
+
+    public List<Slider> optionsSliders = new List<Slider>();
+    public List<TMP_Text> optionsCurrent = new List<TMP_Text>();
+    public List<TMP_Text> optionsHighest = new List<TMP_Text>();
+
+    private void Awake()
+    {
+        if (instance == null)
+        {   
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            SetStartingValues();
+        }
+        else
+        {
+            instance.TurnOn();
+            Destroy(gameObject);
+            SetStartingValues();
+        }
+    }
+    private void Start()
+    {
+        foreach (Slider slider in optionsSliders)
+        {
+            slider.onValueChanged.AddListener(UpdateCurrent);
+        }
+    }
+    public void LoadSettings(gamemanager gameManager)
+    {
+        gameManager.startDifficulty = (int)optionsSliders[(int)SliderOptions.StartDifficulty].value;
+        gameManager.difficultyRampUp = (int)optionsSliders[(int)SliderOptions.DifficultyRampUp].value;
+        gameManager.amountOfRooms = (int)optionsSliders[(int)SliderOptions.AmountOfRooms].value;
+        gameManager.bossFrequncy = (int)optionsSliders[(int)SliderOptions.BossFrequency].value;
+        gameManager.extractionAmount = (int)optionsSliders[(int)SliderOptions.ExtractionAmount].value;
+        gameManager.luckAmount = (int)optionsSliders[(int)SliderOptions.Luck].value;
+    }
+
+    public void TurnOff()
+    {
+        gameObject.SetActive(false);
+    }
+    public void TurnOn()
+    {
+        gameObject.SetActive(true);
+    }    
+    void UpdateCurrent(float _)
+    {
+        for (int i = 0; i < (int)SliderOptions.MAX; i++)
+        {
+            optionsCurrent[i].text = optionsSliders[i].value.ToString("0");
+        }
+    }
+    void SetStartingValues()
+    {
+        for (int i = 0; i < (int)SliderOptions.MAX; i++)
+        {
+            optionsHighest[i].text = optionsSliders[i].maxValue.ToString("0");
+            optionsCurrent[i].text = optionsSliders[i].value.ToString("0");
+        }
+    }
+}
